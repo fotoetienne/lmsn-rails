3ca8db2e0fd38781740e110d46c459cc19b728ac
stephen <root@lmsnhost>
2011-04-19 15:29:57 -0400

    generate scaffold users

---
+++ spec/views/users/edit.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "users/edit.html.erb" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :name => "MyString",
+      :email => "MyString",
+      :password => "MyString",
+      :role => "MyString",
+      :active => false
+    ))
+  end
+
+  it "renders the edit user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => users_path(@user), :method => "post" do
+      assert_select "input#user_name", :name => "user[name]"
+      assert_select "input#user_email", :name => "user[email]"
+      assert_select "input#user_password", :name => "user[password]"
+      assert_select "input#user_role", :name => "user[role]"
+      assert_select "input#user_active", :name => "user[active]"
+    end
+  end
+end
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "users/new.html.erb" do
+  before(:each) do
+    assign(:user, stub_model(User,
+      :name => "MyString",
+      :email => "MyString",
+      :password => "MyString",
+      :role => "MyString",
+      :active => false
+    ).as_new_record)
+  end
+
+  it "renders new user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => users_path, :method => "post" do
+      assert_select "input#user_name", :name => "user[name]"
+      assert_select "input#user_email", :name => "user[email]"
+      assert_select "input#user_password", :name => "user[password]"
+      assert_select "input#user_role", :name => "user[role]"
+      assert_select "input#user_active", :name => "user[active]"
+    end
+  end
+end
