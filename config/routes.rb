86729172705613bde3f31dd2cc61bdaee47fdafe
stephen <root@lmsnhost>
2011-04-19 16:06:19 -0400

    copied some views from sample_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 LmsnRails::Application.routes.draw do
-  resources :users
+
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
+
+  resources :users # remove later
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
