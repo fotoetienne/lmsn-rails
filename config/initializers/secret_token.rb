fc20d49666661b89ca713f5cde173399a95d96b3
stephen <root@lmsnhost>
2011-04-19 12:21:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LmsnRails::Application.config.secret_token = '996013fad9a241c31b0ce9a38798827db696119f67b68fa48978c26f79af419e78450d5645d1bb3f6e33672c7656712f1b7fa758e91e7f0bec0cbf79610bf096'
